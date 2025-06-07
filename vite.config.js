ac4da4652eae813f7a45d4e2514ffc4c5de0986c
IshaanGarg-Github <ishaangarg2289@gmail.com>
2025-06-07 19:35:49 +0530

    Added some vercel deployement code

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/web_4_Veer_Homeopathy_Store-main-",
+  base: "/",
 });
